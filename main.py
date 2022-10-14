edbde50818258a44ee38621333128a2d85b8b0a9
pandora099 <115810897+pandora099@users.noreply.github.com>
2022-10-14 12:35:06 -0400

    Update main.py
    Added stuffs

--- main.py
+++ main.py
@@ -1,2 +1,46 @@
 print("hello");
 print("bhagya")
+
+import sys
+print(sys.version)
+
+print("Hello World")
+
+my_name = "Shree"
+print("hello",my_name)
+
+my_age = 26
+print(my_age)
+
+str_1 = "\"TUjhya aaicha gho\""
+print(sys.maxsize)
+
+print(sys.float_info.max)
+f1 = 1.11111111111111111
+f2 = 1.11111111111111111
+f3 = f1 + f2
+print(f3)
+
+can_vote = True
+
+print("cast", type(int(5.4)))
+print("cast 1", type(str(5.4)))
+print("cast 2", type(chr(97)))
+print("cast 3", type(ord('a')))
+print("cast 4", type(float(2)))
+
+from datetime import datetime
+
+print(datetime.now())
+
+age = 2
+Age = 3
+print(Age)
+
+num_1 = "1"
+num_2 = "2"
+
+print("1 + 2 =", (int(num_1) + int(num_2)))
+
+name = input("What's your name?")
+print("hey", name)
